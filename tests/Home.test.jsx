ce0ab8cd07829ea75aaf51b9a4a771efa3a16c8b
ruizterce <127963868+ruizterce@users.noreply.github.com>
2024-10-01 14:12:37 +0200

    Update Home.test.jsx

--- tests/Home.test.jsx
+++ tests/Home.test.jsx
@@ -2,11 +2,11 @@ import { render, screen } from "@testing-library/react";
 import Home from "../src/components/Home";
 
 describe("Home", () => {
-  it("renders Home component", () => {
+  it("renders Home component with the landing image", () => {
     render(<Home />);
 
-    // Check if "Home" text is rendered
-    const homeText = screen.getByText(/Home/i);
-    expect(homeText).toBeInTheDocument();
+    // Check if the image with the correct alt text is rendered
+    const landingImage = screen.getByAltText(/Landing page image/i);
+    expect(landingImage).toBeInTheDocument();
   });
 });
