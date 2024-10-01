c0f8475f11fa44dacd874d668a3bf997396de798
ruizterce <127963868+ruizterce@users.noreply.github.com>
2024-10-01 13:44:26 +0200

    Add landing image to Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,10 @@
 import styles from "./Home.module.css";
+import landingImage from "../assets/images/landing_main.jpeg";
 
 export default function Home() {
-  return <div className={styles.home}>Home</div>;
+  return (
+    <div className={styles.home}>
+      <img src={landingImage} alt="Landing page image" />
+    </div>
+  );
 }
