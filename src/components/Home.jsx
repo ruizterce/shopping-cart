4634a0e00935af71d65c2774ddd217af057be745
ruizterce <127963868+ruizterce@users.noreply.github.com>
2024-10-01 15:32:42 +0200

    Add a Link to Shop in Home, update styles and test

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
 import landingImage from "../assets/images/landing_main.jpeg";
-
 export default function Home() {
   return (
     <div className={styles.home}>
       <img src={landingImage} alt="Landing page image" />
+
+      <Link to="/shop" className={styles.link}>
+        <p>Shop</p>
+        <p>Now!</p>
+      </Link>
     </div>
   );
 }
--- tests/Home.test.jsx
+++ tests/Home.test.jsx
@@ -1,12 +1,27 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom"; // Import MemoryRouter to provide routing context
 import Home from "../src/components/Home";
 
 describe("Home", () => {
-  it("renders Home component with the landing image", () => {
-    render(<Home />);
+  it("renders Home component with the landing image and link to shop", () => {
+    render(
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>,
+    );
 
     // Check if the image with the correct alt text is rendered
     const landingImage = screen.getByAltText(/Landing page image/i);
     expect(landingImage).toBeInTheDocument();
+
+    // Check if the link to the shop is rendered
+    const shopLink = screen.getByText(/Shop/i);
+    expect(shopLink).toBeInTheDocument();
+
+    // Check if the link has the correct text
+    expect(screen.getByText(/Now!/i)).toBeInTheDocument();
+
+    // Check if the link has the correct URL
+    expect(shopLink.closest("a")).toHaveAttribute("href", "/shop");
   });
 });
